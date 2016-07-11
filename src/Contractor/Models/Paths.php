a616b8ed90b0c4a1bc4823a9a4a18504fea584fc
Austin Kregel <austin@kbcomp.co>
2016-07-11 15:54:32 -0400

    Update paths

--- src/Contractor/Models/Paths.php
+++ src/Contractor/Models/Paths.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: sodium-chloride
- * Date: 4/20/2016
- * Time: 5:05 PM
- */
-
 namespace Kregel\Contractor\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
